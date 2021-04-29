fd13f6804dfcc825da0c854af4f7d069b12f5366
root <root@root.com>
2021-04-29 00:00:09 +0000

    tashe me ip

--- _01_drawing/_a_shapes_and_colors.py
+++ _01_drawing/_a_shapes_and_colors.py
@@ -5,32 +5,54 @@ if __name__ == '__main__':
     window.bgcolor('white')
 
     # This code makes a new Turtle. Pick a new name for the turtle
-    my_turtle = turtle.Turtle()
+    Shivam = turtle.Turtle()
 
     # Make your turtle's shape 'turtle', .shape('turtle')
-
+    Shivam.shape('turtle')
     # Set your turtle's speed using .speed(2)
-
+    Shivam.speed(2)
     # Set your turtle's color using .color('green') and .pencolor('blue')
-
+    Shivam.color('green')
     # Move your turtle forward using .forward(100)
     # TEST    Did your turtle move forward?
-
+    Shivam.forward(100)
     # Move your turtle left or right using .left(90) or .right(90)
-
+    Shivam.right(90)
     # Now put the forward and left/right code into a for loop to repeat 4 times.
-    # TEST    Did your turtle draw a square?
+    # TEST    Did your turtle draw a square
+    for i in range(1,4):
+        Shivam.forward(100)
+        Shivam.right(90)
 
     # Move your turtle to a new place on the screen using .goto(x, y)
     # x=0 and y=0 is the center of the screen
-
+    Shivam.goto(150 ,150)
     # Have your turtle draw a circle using .circle(radius, steps=30)
     # TEST    Did your turtle draw a circle?
+    Shivam.begin_fill()
+    Shivam.circle(50, 130)
+    Shivam.end_fill()
 
     # Add color to your shape by adding .begin_fill() before drawing the circle
     # and .end_fill() below
 
     # Draw 3 more shapes with different fill colors!
-
+    Shivam.penup()
+    Shivam.forward(50)
+    Shivam.begin_fill()
+    Shivam.pendown()
+    Shivam.right(60)
+    Shivam.forward(50)
+    Shivam.right(60)
+    Shivam.forward(50)
+    Shivam.right(60)
+    Shivam.forward(50)
+    Shivam.right(60)
+    Shivam.forward(50)
+    Shivam.right(60)
+    Shivam.forward(50)
+    Shivam.right(60)
+    Shivam.forward(50)
+    Shivam.end_fill()
     # ===================== DO NOT EDIT THE CODE BELOW ============================
     turtle.done()
--- _01_drawing/_b_tashe_me.py
+++ _01_drawing/_b_tashe_me.py
@@ -35,7 +35,7 @@ def screen_clicked(x, y):
 
     # 4. Show your moustache by calling the .showturtle() function
     # moustache.showturtle()
-
+    moustache.showturtle()
     # 5. Move your moustache to a new location using .goto(x, y)
 
 
@@ -46,11 +46,11 @@ if __name__ == '__main__':
     #    add the file to the folder with your code
 
     # 2. Call the set_background() function with the image filename inside of the parenthesis
-    set_background('emoji.png')
+    set_background('emoji2.gif')
 
     # 3. Create a variable called moustache and set it equal to add_moustache('moustache1.gif')
     # moustache = add_moustache('moustache1.gif')
-
+    moustache = add_moustache('emoji2.gif')
     # ===================== DO NOT EDIT THE CODE BELOW ============================
     window.onclick(screen_clicked)
     turtle.done()
